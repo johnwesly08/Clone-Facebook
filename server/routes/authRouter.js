e056b08ed2030ddda5f09316f88c1c4ecbca3e38
johnwesly08 <johnwesleywork08@gmail.com>
2025-01-15 23:09:52 +0530

    Bug fixes

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -2,7 +2,6 @@ const express = require('express');
 const verifyToken = require('../middleware/verifyToken');
 const {registerUser, loginUser, getUserProfile} = require('../controllers/authController');
 
-
 const router = express.Router();
 
 router.post('/register',registerUser);
