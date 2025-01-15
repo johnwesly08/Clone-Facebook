e44891583c4f31b2bfea885207c754585f5a7297
johnwesly08 <johnwesleywork08@gmail.com>
2025-01-15 22:59:02 +0530

    Bug Fixes

--- server/server.js
+++ server/server.js
@@ -1,41 +1,33 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const authRoutes = require('./routes/authRouter');
-const connectDB = require('./config/db');
+const express = require("express");
+const mongoose = require("mongoose");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRouter");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 //cors
-app.use(cors());
-
-app.use(cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST'],
-    credentials: true,
-}));
+app.use(cors(corsOptions));
 
 //Helmet for security
-app.use(helmet({ 
-    contentSecurityPolicy: false,
-    referrerPolicy: {policy: 'strict-origin-when-cross-origin'},
-}));
+app.use(helmet());
 
 //middleware
 app.use(express.json());
 
-// routes 
-app.use('/api/auth', authRoutes);
+// routes
+app.use("/api/auth", authRoutes);
 
 //MongoDB connection
 connectDB();
 
 //Start the server
 app.listen(PORT, () => {
-    console.log(`Server running on PORT ${PORT}`);
+  console.log(`Server running on PORT ${PORT}`);
 });
