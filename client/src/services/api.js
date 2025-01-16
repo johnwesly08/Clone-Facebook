c2dca0da0b5663c1e0f9f538e19f815933628d9a
johnwesly08 <johnwesleywork08@gmail.com>
2025-01-16 23:48:21 +0530

    Axios Api is completed

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,11 +1,23 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({ baseURL: "http://localhost:5000/api/auth" });
 
 export const registerUser = async (UserData) => {
-  return axios.post("auth/register", UserData);
+  try {
+    const response = await API.post("auth/register", UserData);
+    return response;
+  } catch (error) {
+    console.error("Error during Registration", error.message);
+    throw error;
+  }
 };
 
 export const loginUser = async (UserData) => {
-  return axios.post("auth/login", UserData);
+  try {
+    const response = await API.post("auth/login", UserData);
+    return response;
+  } catch (error) {
+    console.error("Error during Login", error.message);
+    throw error;
+  }
 };
